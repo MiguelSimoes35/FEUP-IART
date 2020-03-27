6497930b7562d7a435b0915f6e059d3da2cd3e96
Miguel Simões <miguelsimoes20000720@gmail.com>
2020-03-27 16:51:36 +0000

    Added Player 2 capture moves

--- Proj1/src/Board.cpp
+++ Proj1/src/Board.cpp
@@ -37,6 +37,7 @@ Board::Board() {
 			game_state[i][j] = Piece(2);
 		}
 	}
+	game_state[2][4] = Piece(1);
 }
 
 void Board::display_board() {
@@ -428,5 +429,131 @@ void Board::capture_player1(int o_line, char o_col, int d_line, char d_col) {
 }
 
 void Board::capture_player2(int o_line, char o_col, int d_line, char d_col) {
-	//TO DO
+	int delta_line = d_line - o_line;
+
+	int d_column = int(d_col) - 65;
+	int o_column = int(o_col) - 65;
+	int delta_col = d_column - o_column;
+
+	switch (delta_line) {
+	case 2: {
+		if (delta_col == -2) {
+			if (game_state[d_line - 1][d_column].getPlayer() == 0) {
+				if (game_state[o_line][d_column + 1].getPlayer() == 1) {
+
+					game_state[o_line - 1][o_column].setPlayer(0);
+					game_state[d_line - 1][d_column].setPlayer(2);
+					game_state[o_line][d_column + 1].setPlayer(0);
+				}
+				else {
+					//error
+					//not capturing opponent's piece
+					cout << "ERROR 41!" << endl;
+				}
+			}
+			else {
+				//error
+				//moving to a non-empty cell...
+				cout << "ERROR 42!" << endl;
+			}
+		}
+		else if (delta_col == 0) {
+			if (game_state[d_line - 1][d_column].getPlayer() == 0) {
+				if (game_state[o_line][d_column].getPlayer() == 1) {
+
+					game_state[o_line - 1][o_column].setPlayer(0);
+					game_state[d_line - 1][d_column].setPlayer(2);
+					game_state[o_line][d_column].setPlayer(0);
+				}
+				else {
+					//error
+					//not capturing opponent's piece
+					cout << "ERROR 43!" << endl;
+				}
+			}
+			else {
+				//error
+				//moving to a non-empty cell...
+				cout << "ERROR 44!" << endl;
+			}
+		}
+		else if (delta_col == 2) {
+			if (game_state[d_line - 1][d_column].getPlayer() == 0) {
+				if (game_state[o_line][d_column - 1].getPlayer() == 1) {
+
+					game_state[o_line - 1][o_column].setPlayer(0);
+					game_state[d_line - 1][d_column].setPlayer(2);
+					game_state[o_line][d_column - 1].setPlayer(0);
+				}
+				else {
+					//error
+					//not capturing opponent's piece
+					cout << "Error 45!" << endl;
+				}
+			}
+			else {
+				//error
+				//moving to a non-empty cell...
+				cout << "ERROR 46!" << endl;
+			}
+		}
+		else {
+			//error
+			//invalid move
+			cout << "ERROR 47!" << endl;
+		}
+		break;
+	}
+	case 0: {
+		if (delta_col == -2) {
+			if (game_state[d_line - 1][d_column].getPlayer() == 0) {
+				if (game_state[d_line - 1][d_column + 1].getPlayer() == 1) {
+
+					game_state[d_line - 1][d_column].setPlayer(2);
+					game_state[d_line - 1][d_column + 1].setPlayer(0);
+					game_state[o_line - 1][o_column].setPlayer(0);
+				}
+				else {
+					//error
+					//not capturing opponent's piece
+					cout << "Error 48!" << endl;
+				}
+			}
+			else {
+				//error
+				//moving to a non-empty cell...
+				cout << "ERROR 49!" << endl;
+			}
+		}
+		else if (delta_col == 2) {
+			if (game_state[d_line - 1][d_column].getPlayer() == 0) {
+				if (game_state[d_line - 1][d_column - 1].getPlayer() == 1) {
+
+					game_state[d_line - 1][d_column].setPlayer(2);
+					game_state[d_line - 1][d_column - 1].setPlayer(0);
+					game_state[o_line - 1][o_column].setPlayer(0);
+				}
+				else {
+					//error
+					//not capturing opponent's piece
+					cout << "Error 50!" << endl;
+				}
+			}
+			else {
+				//error
+				//moving to a non-empty cell...
+				cout << "ERROR 51!" << endl;
+			}
+		}
+		else {
+			//error
+			//invalid line selection
+		}
+		break;
+	}
+	default:
+		//error
+		//invalid move
+		break;
+	}
 }
