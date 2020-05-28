abbfd7c14f7311641302ba0c54a0893e155b1728
safarsaitam <47611246+safarsaitam@users.noreply.github.com>
2020-05-28 22:05:01 +0100

    Added simple menu

--- Proj2/src/nb_main.py
+++ Proj2/src/nb_main.py
@@ -1,18 +1,37 @@
+import sys
+
 import naiveBayes as nb
 import fileHandle as fh
 import stringHandle as sh
 
-filename = 'EI-oc-En-anger-train.txt'
-
+filename = ''
 tweeties = []
 scores = []
 tweetsoh = []
-tweets = []
 
-fh.parse_tweets(filename, tweeties, scores)
+emotion = input("Specify an emotion (possible emotions are anger, sadness, fear and joy): ")
+
+if emotion == 'anger':
+    filename = 'EI-oc-En-anger-train.txt'
+    fh.parse_tweets_anger(filename, tweeties, scores)
+elif emotion == 'sadness':
+    filename = 'EI-oc-En-sadness-train.txt'
+    fh.parse_tweets_sadness(filename, tweeties, scores)
+elif emotion == 'fear':
+    filename = 'EI-oc-En-fear-train.txt'
+    fh.parse_tweets_fear(filename, tweeties, scores)
+elif emotion == 'joy':
+    filename = 'EI-oc-En-joy-train.txt'
+    fh.parse_tweets_joy(filename, tweeties, scores)
+else:
+    print('Wrong input, aborting')
+    sys.exit()
+
 sh.remove_ats(tweeties, tweetsoh)
 
+tweets = []
 sh.stemming(tweetsoh, tweets)
 
-#nb.bag_of_words_multi_stats(tweets, scores)
-nb.bag_of_words_multi_input(tweets, scores)
+# nb.bag_of_words_multi_stats(tweets, scores)
+while True:
+    nb.bag_of_words_multi_input(tweets, scores)
