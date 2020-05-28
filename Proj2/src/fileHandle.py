88620e81e5c9bfdd28fe0bce81a4e541163db0f3
safarsaitam <47611246+safarsaitam@users.noreply.github.com>
2020-05-28 22:04:32 +0100

    Seperated file handle for each emotion

--- Proj2/src/fileHandle.py
+++ Proj2/src/fileHandle.py
@@ -1,7 +1,7 @@
 import re
 
 
-def parse_tweets(filename, tweet, score):
+def parse_tweets_anger(filename, tweet, score):
     fr = open(filename, 'r', encoding="utf8")
     text = fr.readlines()
     fr.close()
@@ -10,3 +10,78 @@ def parse_tweets(filename, tweet, score):
         split = re.split("(\d:)", line)
         tweet.append(split[0][14:-7])
         score.append(split[1][:-1])
+
+    fr = open('2018-EI-oc-En-anger-dev.txt', 'r', encoding="utf8")
+    text = fr.readlines()
+    fr.close()
+
+    for line in text[1:]:
+        split = re.split("(\d:)", line)
+        tweet.append(split[0][14:-7])
+        score.append(split[1][:-1])
+
+
+def parse_tweets_fear(filename, tweet, score):
+    fr = open(filename, 'r', encoding="utf8")
+    text = fr.readlines()
+    fr.close()
+
+    for line in text[1:]:
+        split = re.split("(\d:)", line)
+        tweet.append(split[0][14:-7])
+        score.append(split[1][:-1])
+
+    fr = open('2018-EI-oc-En-fear-dev.txt', 'r', encoding="utf8")
+    text = fr.readlines()
+    fr.close()
+
+    for line in text[1:]:
+        split = re.split("(\d:)", line)
+        tweet.append(split[0][14:-7])
+        score.append(split[1][:-1])
+
+
+def parse_tweets_sadness(filename, tweet, score):
+    fr = open(filename, 'r', encoding="utf8")
+    text = fr.readlines()
+    fr.close()
+
+    for line in text[1:]:
+        split = re.split("(\d:)", line)
+        tweet.append(split[0][14:-7])
+        score.append(split[1][:-1])
+
+    fr = open('2018-EI-oc-En-sadness-dev.txt', 'r', encoding="utf8")
+    text = fr.readlines()
+    fr.close()
+
+    for line in text[1:]:
+        split = re.split("(\d:)", line)
+        tweet.append(split[0][14:-7])
+        score.append(split[1][:-1])
+
+
+def parse_tweets_joy(filename, tweet, score):
+    fr = open(filename, 'r', encoding="utf8")
+    text = fr.readlines()
+    fr.close()
+
+    for line in text[1:]:
+        split = re.split("(\d:)", line)
+        tweet.append(split[0][14:-7])
+        score.append(split[1][:-1])
+
+    fr = open('2018-EI-oc-En-joy-dev.txt', 'r', encoding="utf8")
+    text = fr.readlines()
+    fr.close()
+
+    for line in text[1:]:
+        split = re.split("(\d:)", line)
+        tweet.append(split[0][14:-7])
+        score.append(split[1][:-1])
+
+
+
+
+
+
